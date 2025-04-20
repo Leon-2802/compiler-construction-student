e024dc9e3761a8cb0bfcdce6b3f92419912809a3
Leon Gobbert <leon.gobbert@hs-furtwangen.de>
2025-04-20 21:16:35 +0200

    task 02 passed

--- src/compilers/lang_loop/loop_compiler.py
+++ src/compilers/lang_loop/loop_compiler.py
@@ -25,31 +25,54 @@ def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
 
 def compileStmts(stmts: list[stmt]) -> list[WasmInstr]:
     wasmInstructs: list[WasmInstr] = []
+    loop_counter: dict[str, int] = {}
     for s in stmts:
-         a = matchType(s)
+         a = matchType(s, loop_counter)
          wasmInstructs.extend(a)
     return wasmInstructs
         
 
-def matchType(s: stmt) -> list[WasmInstr]:
+def matchType(s: stmt, loop_counter: dict[str, int]) -> list[WasmInstr]:
     wasmInstructs: list[WasmInstr] = []
     match s:
         case StmtExp(e):
             wasmInstructs.extend(compileExp(e))
         case IfStmt(cond, thenBody, elseBody):
+            wasmInstructs.extend(compileExp(cond))
             thenBodyInstr: list[WasmInstr] = compileStmts(thenBody)
             elseBodyInstr: list[WasmInstr] = compileStmts(elseBody)
-            wasmInstructs.append(WasmInstrIf(mapVarType(tyOfExp(cond)), thenBodyInstr, elseBodyInstr))
+            wasmInstructs.append(WasmInstrIf(None, thenBodyInstr, elseBodyInstr))
         case WhileStmt(cond, body):
-            bodyInstr: list[WasmInstr] = compileStmts(body)
-            wasmInstructs.append(WasmInstrLoop())
+            loop_start_label: str = generateUniqueLabel('$loop_start', loop_counter)
+            loop_exit_label: str = generateUniqueLabel('$loop_exit', loop_counter)
+            blockBodyInstr: list[WasmInstr] = []
+            loopBodyInstr: list[WasmInstr] = []
+
+            # Compile the condition
+            loopBodyInstr.extend(compileExp(cond))
+
+            # Compile the body of the loop
+            ifBodyInstr: list[WasmInstr] = compileStmts(body)
+            ifBodyInstr.append(WasmInstrBranch(WasmId(loop_start_label), False))
+
+            # Handle the else body (exit the loop)
+            elseBodyInstr: list[WasmInstr] = [WasmInstrBranch(WasmId(loop_exit_label), False)]
+
+            # Assemble the loop body instructions
+            loopBodyInstr.append(WasmInstrIf(None, ifBodyInstr, elseBodyInstr))
+            blockBodyInstr.append(WasmInstrLoop(WasmId(loop_start_label), loopBodyInstr))
+
+            # Assemble the block body instructions
+            wasmInstructs.append(WasmInstrBlock(WasmId(loop_exit_label), None, blockBodyInstr))
         case Assign(x, IntConst(n)):
             wasmInstructs.append(WasmInstrConst("i64", n))
             wasmInstructs.append(WasmInstrVarLocal("set", identToWasmId(x)))
         case Assign(x, e):
             wasmInstructs.extend(compileExp(e))
             wasmInstructs.append(WasmInstrVarLocal("set", identToWasmId(x)))
+
     return wasmInstructs
+
 
 def compileExp(e: exp) -> list[WasmInstr]:
     wasmInstructs: list[WasmInstr] = []
@@ -57,30 +80,104 @@ def compileExp(e: exp) -> list[WasmInstr]:
         case IntConst(val):
             wasmInstructs.append(WasmInstrConst('i64', val))
         case BoolConst(val):
-            wasmInstructs.append(WasmInstrConst('i32', val))
+            boolToNumericalVal: int = 0
+            if val == True:
+                boolToNumericalVal = 1
+            wasmInstructs.append(WasmInstrConst('i32', boolToNumericalVal))
         case Name(val):
             wasmInstructs.append(WasmInstrVarLocal("get", identToWasmId(val)))
         case Call(name, args):
-            isInt: bool = False
-            for arg in args:
-                if (mapVarType(tyOfExp(arg)) == 'i64'):
-                    isInt = True
-                wasmInstructs.extend(compileExp(arg))
-            if (isInt == True):
-                wasmInstructs.append(WasmInstrCall(intIdentToWasmId(name)))
-            else:
-                wasmInstructs.append(WasmInstrCall(boolIdentToWasmId(name)))
+            wasmInstructs.extend(compileCall(name, args))
         case UnOp(op, arg):
-            wasmInstructs.append(WasmInstrConst('i64', 0))
-            wasmInstructs.extend(compileExp(arg))
-            wasmInstructs.append(WasmInstrNumBinOp('i64', unaryOpToLiteral(op)))
+            wasmInstructs.extend(compileUnaryOp(op, arg))
         case BinOp(left, op, right):
-            wasmInstructs.extend(compileExp(left))
-            wasmInstructs.extend(compileExp(right))
-            wasmInstructs.append(WasmInstrNumBinOp(mapVarType(tyOfExp(left)), binaryOpToLiteral(op)))
-        # Bool and Int operations: Gleich, Ungleich, print
+            wasmInstructs.extend(compileBinaryOp(left, op, right))
 
     return wasmInstructs
+
+
+def compileCall(name: ident, args: list[exp]) -> list[WasmInstr]:
+    wasmInstructs: list[WasmInstr] = []
+    isBool: bool = any(mapVarType(tyOfExp(arg)) == 'i32' for arg in args)
+    for arg in args:
+        wasmInstructs.extend(compileExp(arg))
+    if isBool == True:
+        wasmInstructs.append(WasmInstrCall(boolIdentToWasmId(name)))
+    else:
+        wasmInstructs.append(WasmInstrCall(intIdentToWasmId(name)))
+    return wasmInstructs
+
+
+def compileUnaryOp(op: unaryop, arg: exp) -> list[WasmInstr]:
+    wasmInstructs: list[WasmInstr] = []
+    match op:
+        case USub():
+            wasmInstructs.append(WasmInstrConst('i64', 0))
+            wasmInstructs.extend(compileExp(arg))
+            wasmInstructs.append(WasmInstrNumBinOp('i64', 'sub'))
+        case Not():
+            wasmInstructs.extend(compileExp(arg))
+            wasmInstructs.append(WasmInstrConst('i32', 1))
+            wasmInstructs.append(WasmInstrNumBinOp('i32', 'xor'))
+
+    return wasmInstructs
+
+
+def compileBinaryOp(left: exp, op: binaryop, right: exp) -> list[WasmInstr]:
+    wasmInstructs: list[WasmInstr] = []
+    match op:
+        case Add():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrNumBinOp('i64', 'add')) 
+        case Sub():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrNumBinOp('i64', 'sub')) 
+        case Mul():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrNumBinOp('i64', 'mul'))
+        case And():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.append(WasmInstrIf('i32', compileExp(right), [WasmInstrConst('i32', 0)]))
+        case Or():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.append(WasmInstrIf('i32', [WasmInstrConst('i32', 1)], compileExp(right)))
+        case Less():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrIntRelOp('i64', 'lt_s'))
+        case LessEq():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrIntRelOp('i64', 'le_s'))
+        case Greater():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrIntRelOp('i64', 'gt_s'))
+        case GreaterEq():
+            wasmInstructs.extend(compileExp(left))
+            wasmInstructs.extend(compileExp(right))
+            wasmInstructs.append(WasmInstrIntRelOp('i64', 'ge_s'))
+        case Eq():
+            if (mapVarType(tyOfExp(left)) != mapVarType(tyOfExp(right))):
+                emptyInstr: list[WasmInstr] = []
+                return emptyInstr
+            else:
+                wasmInstructs.extend(compileExp(left))
+                wasmInstructs.extend(compileExp(right))
+                wasmInstructs.append(WasmInstrIntRelOp(mapVarType(tyOfExp(left)), 'eq')) 
+        case NotEq():
+            if (mapVarType(tyOfExp(left)) != mapVarType(tyOfExp(right))):
+                emptyInstr: list[WasmInstr] = []
+                return emptyInstr
+            else:
+                wasmInstructs.extend(compileExp(left))
+                wasmInstructs.extend(compileExp(right))
+                wasmInstructs.append(WasmInstrIntRelOp(mapVarType(tyOfExp(left)), 'ne')) 
+    return wasmInstructs
+
 
 def mapVarType(var_type: ty) -> Literal['i64', 'i32']:
     """
@@ -95,14 +192,16 @@ def mapVarType(var_type: ty) -> Literal['i64', 'i32']:
 def tyOfExp(e: exp) -> ty:
     match e.ty:
         case None:
-            raise ValueError('exp has type None')
+            raise ValueError('Expression has type None')
         case Void():
-            raise ValueError('exp has type Void')
+            raise ValueError('Expression has type Void')
         case NotVoid(t):
             return t
+        
  
 def identToWasmId(x: ident) -> WasmId:
     return WasmId('$' + x.name)
+
 
 def boolIdentToWasmId(x: ident) -> WasmId:
     if x.name == "print":
@@ -112,27 +211,18 @@ def boolIdentToWasmId(x: ident) -> WasmId:
     else:
         return WasmId('$' + x.name)
     
+    
 def intIdentToWasmId(x: ident) -> WasmId:
     if x.name == "print":
         return WasmId('$print_i64')
     elif x.name == "input_int":
         return WasmId('$input_i64')
     else:
-        return WasmId('$' + x.name)
+        return WasmId('$' + x.name)   
+    
 
-def binaryOpToLiteral(op: binaryop) -> Literal['add', 'sub', 'mul', 'shr_u', 'shl', 'xor']:
-    match op:
-        case Add():
-            return 'add'
-        case Sub():
-            return 'sub'
-        case Mul():
-            return 'mul'
-        case Less():
-
-        
-def unaryOpToLiteral(op: unaryop) -> Literal['add', 'sub']:
-    match op:
-        case USub():
-            return 'sub'
-        case Not():
+def generateUniqueLabel(prefix: str, counter: dict[str, int]) -> str:
+    if prefix not in counter:
+        counter[prefix] = 0
+    counter[prefix] += 1
+    return f"{prefix}_{counter[prefix]}"     
